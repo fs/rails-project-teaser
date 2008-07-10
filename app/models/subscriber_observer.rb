cd744c9b5b6f1da0d1e7612198acf5ac0b654381
Nick Mikhaylov <nick.mikhaylov@flatsoft.ru>
2008-07-10 19:55:19 +0400

    fight with observers

---
+++ app/models/subscriber_observer.rb
@@ -0,0 +1,10 @@
+class SubscriberObserver < ActiveRecord::Observer
+
+    #observe Subscriber
+
+    #def after_create(subscriber)
+    #    Confirmer.deliver_sent :email
+    #    subscriber.logger.warn("!!!!! email #{subscriber.email}")
+    #end
+
+end
